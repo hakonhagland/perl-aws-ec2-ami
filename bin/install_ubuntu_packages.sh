f11f3aa02c630ffa10be33b3845a4da7355a4bdb
Håkon Hægland <hakon.hagland@gmail.com>
2019-07-22 13:50:22 +0200

    Install some ubuntu packages.

---
+++ bin/install_ubuntu_packages.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+curdir=$(dirname "$(readlink -f "$0")")
+pack_fn="$curdir/../ubuntu_packages_to_install.txt"
+echo
+echo "Installing packages.."
+echo "---------------------"
+echo
+while read line; do
+    echo 
+    echo "Installing \"$line\".."
+    echo "--------------------------------------"
+    sleep 0.3
+    sudo apt-get install --yes $line
+done <package_names.txt
